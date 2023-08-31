e9872303df8534b194d76885407e72fc8cef6207
unknown <imane.elbakir@gmail.com>
2023-08-31 10:21:09 +0100

    new commit

---
+++ script.js
@@ -0,0 +1,5 @@
+window.addEventListener*('load', () =>{
+    const box = document.querySelector('.box');
+    const input = document.querySelector('#inputBx');
+    const listElment = document.querySelector('#tasks');
+})
